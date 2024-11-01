32b2dcf97c0216b7845830254dfa9c5d604d39de
Alexis Martinez <a.martinez0018@gmail.com>
2024-10-31 20:18:40 -0700

    finish project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
 import './App.css'
+import { useState } from 'react'
+import {puppyList} from './data.js'
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+
+  // console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup);
+
+  
+
   return (
     <>
-      <div>
-    <h1>Congrats you made this far ! </h1>
+      <div className="App">  <h2>Welcome!</h2>
+
+        {puppies.map((puppy) => {
+          return <p onClick={() => {
+            setFeatPupId(puppy.id)
+          }} key={puppy.id}>{puppy.name}</p>;
+        })
+        }
+       {featPupId && <p>{ featPupId }</p>}
+
+       {featPupId && (
+    <div className="App">
+      <h2>{featuredPup.name}</h2>
+      <ul>
+        <li>Age: {featuredPup.age}</li>
+        <li>Email: {featuredPup.email}</li>
+      </ul>
+    </div>
+  )
+
+  }
+
       </div>
     </>
-  )
+  );
 }
 
 export default App
